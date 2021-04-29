b20b9da6f67ed641c5bfb46b2b39bc2c5344b18f
Loky Mo <>
2021-04-29 03:04:50 +0000

    last change

--- main.sh
+++ main.sh
@@ -2,6 +2,7 @@ echo Please Wait....
 mkdir ../.gem
 cp -r gemBINS/* ../.gem
 export PATH=/home/runner/.gem/ruby/2.5.0/bin:$PATH
+chmod +x /home/runner/.gem/ruby/2.5.0/bin/*
 jekyll -v
 cd site
 bundle exec jekyll serve -P 8080 --host=0.0.0.0
